2ab1e77269e838e2f4d2c2fba56f748db3cc9a53
Vdtry-06 <tunhoipro0306@gmail.com>
2025-09-27 20:31:16 +0700

    add department dto and repo

---
+++ src/main/java/com/spring/human/resource/server/payload/department/DepartmentRequest.java
@@ -0,0 +1,19 @@
+package com.spring.human.resource.server.payload.department;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DepartmentRequest {
+    @NotNull
+    private String departmentName;
+
+    @NotNull
+    private String description;
+}
