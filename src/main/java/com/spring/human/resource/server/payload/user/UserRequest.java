ce8e1abe57124b186fcfbeecd7cfe23b0c8d437f
Vdtry-06 <tunhoipro0306@gmail.com>
2025-09-27 20:17:54 +0700

    add user dto

---
+++ src/main/java/com/spring/human/resource/server/payload/user/UserRequest.java
@@ -0,0 +1,30 @@
+package com.spring.human.resource.server.payload.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class UserRequest {
+    private String username;
+    private String password;
+    private String email;
+    private int roleId;
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private int positionId;
+    private int departmentId;
+
+    @Data
+    @Builder
+    public static class UpdateUserRequest{
+        private String username;
+        private String password;
+        private String email;
+    }
+}
---
+++ src/main/java/com/spring/human/resource/server/payload/user/UserResponse.java
@@ -0,0 +1,15 @@
+package com.spring.human.resource.server.payload.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class UserResponse {
+    private int userId;
+    private String username;
+    private String email;
+    private String roleName;
+}
