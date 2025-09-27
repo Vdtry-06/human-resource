6f12b44404cbdec5b7e26175198df8db8fecd4ab
Vdtry-06 <tunhoipro0306@gmail.com>
2025-09-27 20:13:46 +0700

    update position

--- src/main/java/com/spring/human/resource/server/controllers/PositionController.java
+++ src/main/java/com/spring/human/resource/server/controllers/PositionController.java
@@ -8,42 +8,52 @@ import com.spring.human.lib.utils.StringUtil;
 import com.spring.human.resource.server.payload.position.PositionRequest;
 import com.spring.human.resource.server.payload.position.PositionResponse;
 import com.spring.human.resource.server.services.PositionService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+@Log4j2
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "position")
 @RequestMapping("/positions")
 public class PositionController {
     private final PositionService positionService;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('READ_POSITIONS')")
     public PaginationResponse<PositionResponse> getAllPositionWithConditions(
             @RequestParam(required = false, defaultValue = PagingUtil.DEFAULT_PAGE) int page,
-            @RequestParam(required = false, defaultValue = PagingUtil.DEFAULT_SIZE) int perpage,
+            @RequestParam(required = false, defaultValue = PagingUtil.DEFAULT_SIZE) int prePage,
             @RequestParam(required = false, defaultValue = StringUtil.EMPTY) String search ) {
-        return positionService.getPositionWithConditions(page, perpage, search);
+        return positionService.getPositionWithConditions(page, prePage, search);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('READ_POSITIONS')")
     public ApiResponse<PositionResponse> getPositionById(@PathVariable("id") int id) {
         PositionResponse response = positionService.getPositionById(id);
         return new ApiResponse<PositionResponse>(true, response);
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('WRITE_POSITIONS')")
     public ApiResponse<PositionResponse> createPosition(@RequestBody PositionRequest positionRequest) {
         PositionResponse response = positionService.createPosition(positionRequest);
         return new ApiResponse<PositionResponse>(true, response);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('WRITE_POSITIONS')")
     public ApiResponse<PositionResponse> updatePosition(@PathVariable("id") int id, @RequestBody PositionRequest positionRequest) throws Exception {
         PositionResponse response = positionService.updatePosition(id, positionRequest);
         return new ApiResponse<PositionResponse>(true, response);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('DELETE_POSITIONS')")
     public ApiResponse<String> deletePosition(@PathVariable("id") int id) throws Exception {
         positionService.deletePosition(id);
         return new ApiResponse<String>(true);
